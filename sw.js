37fe8ec25032fc38b61ef0e17a37432f11c5b6d5
joeyadame <121279018+joeyadame@users.noreply.github.com>
2026-09-13 17:29:10 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'usps-days-off-v2';
+const CACHE = 'usps-days-off-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
